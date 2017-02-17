3ccf5395f8e5bf6718ef42729e6e5e2b708ba265
luca tiburzio <luca.tiburzio@gmail.com>
2017-02-17 23:42:29 +0100

    lineage update
    Change-Id: I726ba46174141e02b6f76c55b3b9b3b824abeb53

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,6 +38,7 @@ TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_hlte_eur_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/hlte
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+#TARGET_KERNEL_CROSS_COMPILE_PREFIX := armv7a-hardfloat-linux-gnueabi-
 
 # Audio
 QCOM_CSDCLIENT_ENABLED := false
